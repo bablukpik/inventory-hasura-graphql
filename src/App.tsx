6c9fffc51a99c41610f6649bce93fd8d3f1e5c01
Bablu Ahmed <bablukpik@gmail.com>
2023-09-28 03:18:18 +0600

    main: products fetching and showing with table and card done

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,41 @@
-import React, { useState } from 'react';
-import './App.css';
-
-import { Layout, Typography } from 'antd';
-
-
-import { ApolloClient, ApolloProvider, InMemoryCache, HttpLink } from '@apollo/client';
-import { Products } from './Products';
-
+import { useState } from "react";
+import { Layout, Typography } from "antd";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  HttpLink,
+} from "@apollo/client";
+import { Products } from "./Products";
+import "./App.css";
 
 const { Header, Content } = Layout;
 const { Title } = Typography;
+const GRAPHQL_API_URI =
+  process.env.GRAPHQL_API_URI || "http://localhost:8080/v1/graphql";
 
- const createApolloClient = () => {
+const createApolloClient = () => {
   return new ApolloClient({
     link: new HttpLink({
-      uri: 'http://localhost:8080/v1/graphql',
+      uri: GRAPHQL_API_URI,
     }),
     cache: new InMemoryCache(),
   });
- };
+};
 
 function App() {
-
   const [client] = useState(createApolloClient());
 
   return (
     <ApolloProvider client={client}>
       <div className="App">
-        <Layout style={{ height: '100vh' }}>
-          <Header style={{ display: 'flex', alignItems: 'center'}}>
-            <Title style={{ color: 'white', margin: 0, textAlign: 'left' }}>Inventory App</Title>
+        <Layout style={{ height: "100vh" }}>
+          <Header style={{ display: "flex", alignItems: "center" }}>
+            <Title style={{ color: "white", margin: 0, textAlign: "left" }}>
+              Inventory App
+            </Title>
           </Header>
-          <Content style={{ padding: '1em'}}>
+          <Content style={{ padding: "1em" }}>
             <Products />
           </Content>
         </Layout>
--- src/Products.tsx
+++ src/Products.tsx
@@ -1,24 +1,127 @@
-import { gql, useQuery } from '@apollo/client';
-import Statistic from 'antd/es/statistic/Statistic';
-import React from 'react';
+import { gql, useQuery } from "@apollo/client";
+import Statistic from "antd/es/statistic/Statistic";
+import { Table, Button, Card, Row, Col, Space } from "antd";
 
-const countQuery = gql`
-  query CountQuery {
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+}
+
+const GET_PRODUCTS_WITH_AGGREGATE = gql`
+  query GetProductsWithAggregate {
     products_aggregate {
       aggregate {
+        sum {
+          stock
+        }
         count
       }
     }
+    products {
+      id
+      name
+      description
+      price
+      stock
+    }
   }
-`
+`;
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Price",
+    dataIndex: "price",
+    key: "price",
+  },
+  {
+    title: "Stock",
+    dataIndex: "stock",
+    key: "stock",
+  },
+  {
+    title: "Value",
+    dataIndex: "value",
+    key: "value",
+  },
+];
 
 export function Products(): JSX.Element {
+  const { loading, error, data } = useQuery(GET_PRODUCTS_WITH_AGGREGATE, {
+    fetchPolicy: "cache-and-network",
+  });
 
-  const { data } = useQuery(countQuery);
+  const totalCount = data?.products_aggregate?.aggregate?.count ?? 0;
+  const totalStock = data?.products_aggregate?.aggregate?.sum?.stock ?? 0;
+  const totalValue =
+    data?.products?.reduce?.((acc: number, product: Product) => {
+      return acc + product?.stock * product?.price;
+    }, 0) ?? 0;
+
+  const rows: Product[] =
+    data?.products?.map?.((product: Product) => ({
+      key: product?.id,
+      name: product?.name,
+      price: `$${product?.price}`,
+      stock: product?.stock,
+      value: `$${product?.stock * product?.price}`,
+    })) || [];
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
 
   return (
-    <div>
-      <Statistic title="Total products" value={data?.products_aggregate?.aggregate.count} />
-    </div>
+    <>
+      <Row gutter={[16, 16]}>
+        <Col span={8}>
+          <Card>
+            <Statistic
+              title="Total products"
+              value={`$${totalCount}`}
+              loading={loading}
+            />
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card>
+            <Statistic
+              title="Total Stock"
+              value={`$${totalStock}`}
+              loading={loading}
+            />
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card>
+            <Statistic
+              title="Total Value"
+              value={`$${totalValue}`}
+              loading={loading}
+            />
+          </Card>
+        </Col>
+      </Row>
+      <br />
+      <Card>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <h2>Products</h2>
+          <Button type="primary">Add Products</Button>
+        </div>
+        <Table columns={columns} dataSource={rows} loading={loading} />
+        {/* <AddUserModal
+          showForm={open}
+          onCancel={handleCancelForm}
+          onAddUser={handleAddUser}
+        /> */}
+      </Card>
+    </>
   );
 }
